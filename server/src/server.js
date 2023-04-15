e8778ba911b644f84bde3b55c8c01bfe7d1020b3
Admin <akshatchawla1997@gmail.com>
2023-04-15 06:15:38 +0530

    keplers project updated

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,14 @@
+const http = require('http')
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080;
-app.listen(PORT,()=>{
-    console.log(`listening on port number ${PORT}`)
-})
+const app = require('./app')
+const planetsmodel = require('./Models/planets.models')
 
-console.log(PORT)
+const PORT = process.env.PORT || 8080;
+const server = http.createServer(app)
+async function startServer(){
+    await planetsmodel.loadPlanetsData;
+    app.listen(PORT,()=>{
+        console.log(`listening on port number ${PORT}`)
+    })
+}
+startServer();
